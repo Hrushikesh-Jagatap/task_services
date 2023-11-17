31d51fdc2119c4ad08f29e21c5ee4aba32b0aa6c
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-11-17 17:40:37 +0530

    Update index.js

--- src/apis/services/v1/getquebyteacherid/index.js
+++ src/apis/services/v1/getquebyteacherid/index.js
@@ -6,8 +6,7 @@ const getquebyteacherid = async (teacherids) => {
   try {
     console.log(teacherids);
   
-        const Que = await QueData.find({" req_teacherid.teacherid": teacherids });
-
+   const Que = await QueData.find({" req_teacherid.teacherid": teacherids ,}).sort({createdAt:-1})
     return Que;
   } catch (error) {
     throw new Error('Failed to get que by teacherid');

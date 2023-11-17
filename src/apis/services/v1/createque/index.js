a566c2fd8d9d8de9325bc63bd5980a313508d49a
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-11-17 17:40:01 +0530

    Update index.js

--- src/apis/services/v1/createque/index.js
+++ src/apis/services/v1/createque/index.js
@@ -1,7 +1,25 @@
 const Que = require('@models/que');
 const axios = require('axios');
-const { loadBalancer, SYSTEM_TOKEN ,teacher} = require('@config');
-
+const { loadBalancer, SYSTEM_TOKEN ,teacher,student} = require('@config');
+const getStudent = async (sid_userId) => {
+  try {
+    const config = {
+      method: 'get',
+      url: `${student}/sts/apis/v1/user/${sid_userId}`,
+      headers: {
+        app_name: 'studentApp',
+        app_version_code: '101',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${SYSTEM_TOKEN}`,
+      },
+    };
+    const result = await axios(config);
+    return result.data;
+  } catch (error) {
+    console.error('Error fetching student data:', error.message);
+    return null;
+  }
+};
 // Function to make a GET request to retrieve teacher information
 const getTeacher = async (args) => {
   try {
@@ -47,17 +65,36 @@ const createQue = async (queData) => {
   return "no active teacher";
     }
     const userIds = teacherInfo.data.map(obj => obj.userId);
-    console.log('User Ids:', userIds)
-if(userIds==null||userIds.length==0||userIds==undefined)
-{
-    const createdQue = await Que.create(queData);
-  return "no active teacher";
-}
-let n= userIds.length;
-// for(let i=0; i<n; i++){
-const req_teacherid = userIds.map(teacherid => ({ teacherid, status: 'requested', flag: true }));
+//     console.log('User Ids:', userIds)
+// if(userIds==null||userIds.length==0||userIds==undefined)
+// {
+//     const createdQue = await Que.create(queData);
+//   return "no active teacher";
+// }
+// let n= userIds.length;
+// // for(let i=0; i<n; i++){
+// const req_teacherid = userIds.map(teacherid => ({ teacherid, status: 'requested', flag: true }));
 
     // Assign req_teacherid to queData
+
+     if (!userIds || userIds.length === 0) {
+      const createdQue = await Que.create(queData);
+      return "no active teacher";
+    }
+    const n = userIds.length;
+    const req_teacherid = userIds.map(teacherid => ({ teacherid, status: 'requested', flag: true }));
+    // Add name and profileimage to each req_teacherid element
+    for (let i = 0; i < n; i++) {
+      const teacherId = userIds[i];
+      const abc = await getStudent(studentid);
+      const name = abc.data[0].personalDetails?.first_name;
+      const profileimage = abc.data[0].personalDetails?.profileImage;
+      const matchingReqTeacherId = req_teacherid.find(reqTeacherId => reqTeacherId.teacherid === teacherId);
+      if (matchingReqTeacherId) {
+        matchingReqTeacherId.name = name;
+        matchingReqTeacherId.profileimage = profileimage;
+      }
+    }
     queData.req_teacherid = req_teacherid;
 const createdQue = await Que.create(queData);
 // }
